8f676b361fd86ae0593db756dd19132fdf78f790
teutzue_1995@yahoo.com <teutzue_1995@yahoo.com>
2017-05-08 13:24:37 +0200

    initial commit for travis

---
+++ DBproject/src/main/java/core/DBproject/functionality.java
@@ -0,0 +1,21 @@
+package core.DBproject;
+
+/**
+ * Created by CosticaTeodor on 08/05/2017.
+ */
+public class functionality {
+
+    private functionality() {
+        // Should not be instantiated
+    }
+
+    /**
+     * Aaaaaalways returns 10. Or does it?!
+     *
+     * @return 10. Maybe.
+     */
+    public static int giveMeTen() {
+        return 5;
+    }
+
+}
---
+++ DBproject/src/test/java/core/DBproject/testFunct.java
@@ -0,0 +1,17 @@
+package core.DBproject;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by CosticaTeodor on 08/05/2017.
+ */
+public class testFunct {
+
+    @Test
+    public void testFunctionality() {
+        assertEquals(10, functionality.giveMeTen());
+    }
+
+}
