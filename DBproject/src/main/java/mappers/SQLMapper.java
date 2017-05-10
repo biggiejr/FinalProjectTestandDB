a357c30706bdd90e42d379d6929f2b0a55693d7b
biggiejr <jrmacej@gmail.com>
2017-05-10 11:22:14 +0200

    Added mappers
    mappers for both DBs

---
+++ DBproject/src/main/java/mappers/SQLMapper.java
@@ -0,0 +1,29 @@
+package utilities;
+
+public interface SQLMapper {
+
+    /**
+     * Given a city name this method returns all book titles with corresponding authors that mention this city.
+     * @return ArrayList <Book>
+     */
+    public ArrayList <Book> getBookByCity(String city);
+
+    /**
+     * Given a book title, this method plots all cities mentioned in this book.
+     * @return ArrayList <City>
+     */
+    public ArrayList <City> getMentionedCitiesByBook(String bookTitle);
+
+    /**
+     * Given an author name this method lists all books written by that author and plots all cities mentioned
+     * in any of the books.
+     * @return ArrayList <Book>
+     */
+    public ArrayList <Book> getMentionedCitiesByAuthor(String author);
+
+    /**
+     * Given a geolocation, this method lists all books mentioning a city in vicinity of the given geolocation.
+     * @return ArrayList <Book>
+     */
+    public ArrayList <Book> getAllBooksByCity(String location);
+}
